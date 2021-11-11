58b0ec9f263123ca6c8d22f8585aa71eb0f2bd3d
vitalii vitkovskyi <filosoff13@gmail.com>
2021-11-11 17:57:22 +0200

    Lesson#5: Deleted comments in migrations files

--- migrations/Version20211107174143.php
+++ migrations/Version20211107174143.php
@@ -7,25 +7,15 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20211107174143 extends AbstractMigration
 {
-    public function getDescription(): string
-    {
-        return '';
-    }
-
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE task ADD done TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE task DROP done');
     }
 }
