1e35120f83ff06726d8820a0f0f23e3da1e91c99
vitalii vitkovskyi <filosoff13@gmail.com>
2021-12-05 23:47:20 +0200

    lesson#8: Created user entity & registration

---
+++ src/Controller/UserController.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Enum\FlashMessagesEnum;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @Route("/user", name="user_")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/registration", name="registration", methods={"POST"})
+     */
+    public function registration(Request $request, UserPasswordHasherInterface $passwordHasher, ValidatorInterface $validator, EntityManagerInterface $em): Response
+    {
+        $username = $request->request->get('username');
+        $plainPassword = $request->request->get('password');
+        /** @var ConstraintViolationList $passwordErrors */
+        $passwordErrors = $validator->validate($plainPassword, [
+            new Assert\NotBlank(['message' => "Password should not be blank"]),
+            new Assert\Length(['min' => 8, 'max' => 30, 'minMessage' => "Your password must be at least {{ limit }} characters long",
+            'maxMessage' => "Your password cannot be longer than {{ limit }} characters"])
+        ]);
+
+        if ($passwordErrors->count()){
+            foreach ($passwordErrors as $error){
+                $this->addFlash(FlashMessagesEnum::FAIL, $error->getMessage());
+            }
+
+            return $this->redirectToRoute('page_home');
+        }
+
+        $user = new User($username);
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plainPassword
+        );
+
+        $user->setPassword($hashedPassword);
+
+        /** @var ConstraintViolationList $userErrors */
+        $userErrors = $validator->validate($user);
+
+        foreach ($userErrors as $error){
+            $this->addFlash(FlashMessagesEnum::FAIL, $error->getMessage());
+        }
+
+        if (!$userErrors->count()){
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash(FlashMessagesEnum::SUCCESS, 'You have been registered');
+        }
+
+        return $this->redirectToRoute('page_home');
+    }
+}
