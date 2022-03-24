687af5d76aa6bd7937dc3170f7d7a94121b3e6e1
Vitalii V <filosoff13@gmail.com>
2022-03-24 18:26:55 +0200

    Lesson#20 Implemented users list action

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -38,7 +38,7 @@ class UserController extends AbstractController
      *
      * @IsGranted("ROLE_ADMIN")
      */
-    public function list(Request $request, UserService $userService): Response
+    public function list(UserService $userService): Response
     {
         $users = $userService->getUserList();
 
