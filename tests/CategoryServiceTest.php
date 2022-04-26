af18ea9848fcd09e12d63a8d45bdcb2f33d071a2
Vitalii V <filosoff13@gmail.com>
2022-04-26 21:35:45 +0300

    Lesson#22 Implemented CategoryServiceTest

---
+++ tests/CategoryServiceTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Category;
+use App\Exception\ValidationException;
+use App\Service\CategoryService;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class CategoryServiceTest extends TestCase
+{
+    private CategoryService $categoryService;
+    private MockObject $mockObject;
+
+    protected function setUp(): void
+    {
+        $this->mockObject = $this->createMock(ValidatorInterface::class);
+        $this->userService = new CategoryService(
+            $this->mockObject,
+            $this->createMock(EntityManagerInterface::class)
+        );
+    }
+
+    public function test_create_validationFailed_exceptionThrown(): void
+    {
+        $this->mockObject->method('validate')->willReturn(new ConstraintViolationList([
+            new ConstraintViolation('test_error', null, [], null, null, null)
+        ]));
+
+        $this->expectException(ValidationException::class);
+        $this->userService->createAndFlush('');
+    }
+
+    public function test_create_validationSuccess_returnCategory(): void
+    {
+        $this->mockObject->method('validate')->willReturn(new ConstraintViolationList([]));
+        $category = $this->userService->createAndFlush('test_category');
+
+        $this->assertInstanceOf(Category::class, $category);
+        $this->assertEquals('test_category', $category->getTitle());
+    }
+}
